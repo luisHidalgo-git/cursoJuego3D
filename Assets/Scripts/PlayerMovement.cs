0ea07eedc72892e5e8c19d786362a58ad3e4fb11
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-10-15 21:34:13 -0600

    movimiento funcional (mejorable)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
     public float speed = 5f;
     public float gravity = -20f; // Aumentamos la gravedad para una caída más natural
     public float jumpHeight = 1.5f;
@@ -15,18 +14,15 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    void Start()
-    {
+    void Start(){
         controller = GetComponent<CharacterController>();
     }
 
-    void Update()
-    {
+    void Update(){
         // --- Detección del suelo ---
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
-        {
+        if (isGrounded && velocity.y < 0){
             velocity.y = -1f; // Pequeño valor negativo para mantener al jugador pegado al suelo
         }
 
@@ -38,8 +34,7 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
 
         // --- Saltar ---
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
+        if (Input.GetButtonDown("Jump") && isGrounded){
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
