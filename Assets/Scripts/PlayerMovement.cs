5f8c1dd5781644368dad0fc774d9034f0ffa4608
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-10-18 18:51:49 -0600

    animaciones añadidas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerMovement : MonoBehaviour{
-    public float speed = 5f;
-    public float gravity = -20f; // Aumentamos la gravedad para una caída más natural
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
+    public float gravity = -20f;
     public float jumpHeight = 1.5f;
 
     private CharacterController controller;
+    private Animator animator;
     private Vector3 velocity;
     private bool isGrounded;
 
@@ -14,31 +17,45 @@ public class PlayerMovement : MonoBehaviour{
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    void Start(){
+    void Start()
+    {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    void Update(){
+    void Update()
+    {
         // --- Detección del suelo ---
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0){
-            velocity.y = -1f; // Pequeño valor negativo para mantener al jugador pegado al suelo
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            animator.SetBool("isJumping", false);
         }
 
-        // --- Movimiento horizontal ---
+        // --- Movimiento ---
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
 
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
 
-        // --- Saltar ---
-        if (Input.GetButtonDown("Jump") && isGrounded){
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        // --- Animación ---
+        float speedValue = move.magnitude * currentSpeed;
+        animator.SetFloat("Speed", speedValue);
+
+        // --- Salto ---
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            animator.SetBool("isJumping", true);
         }
 
-        // --- Aplicar gravedad ---
+        // --- Gravedad ---
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
